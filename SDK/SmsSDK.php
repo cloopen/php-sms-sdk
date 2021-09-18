ea3a9363349045367b3202c7b862426e2c0f4045
Pang Yingjie <pyjyingjie@gmail.com>
2021-09-18 15:52:08 +0800

    代码更新

--- SDK/SmsSDK.php
+++ SDK/SmsSDK.php
@@ -15,201 +15,194 @@
 
 class REST
 {
-    private $AccountSid;
-    private $AccountToken;
-    private $AppId;
-    private $ServerIP;
-    private $ServerPort;
-    private $SoftVersion;
-    private $Batch;  //时间sh
-    private $BodyType = "xml";//包体格式，可填值：json 、xml
-    private $enabeLog = true; //日志开关。可填值：true、
-    private $Filename = "../log.txt"; //日志文件
-    private $Handle;
+    private $accountSid;
+    private $accountToken;
+    private $appId;
+    private $serverHost;
+    private $serverPort;
+    private $apiHost;
+    private $apiVersion;
+    private $datetime;               // 时间sh
+    private $bodyType = "xml";       // 包体格式，可填值：json 、xml
+    private $logEnabled = true;      // 日志开关。可填值：true、
+    private $logPath = "../log.txt"; // 日志文件
+    private $handle;
 
-    function __construct($ServerIP, $ServerPort, $SoftVersion)
+    function __construct($serverHost, $serverPort, $apiVersion)
     {
-        $this->Batch = date("YmdHis");
-        $this->ServerIP = $ServerIP;
-        $this->ServerPort = $ServerPort;
-        $this->SoftVersion = $SoftVersion;
-        $this->Handle = fopen($this->Filename, 'a');
+        $this->datetime = date("YmdHis");
+        $this->serverHost = $serverHost;
+        $this->serverPort = $serverPort;
+        $this->apiVersion = $apiVersion;
+        $this->apiHost = "https://{$serverHost}:{$serverPort}/{$apiVersion}";
+
+        $this->handle = fopen($this->logPath, 'a');
     }
 
     /**
      * 设置主帐号
      *
-     * @param AccountSid 主帐号
-     * @param AccountToken 主帐号Token
+     * @param string $accountSid 主帐号
+     * @param string $accountToken 主帐号Token
      */
-    function setAccount($AccountSid, $AccountToken)
+    function setAccount($accountSid, $accountToken)
     {
-        $this->AccountSid = $AccountSid;
-        $this->AccountToken = $AccountToken;
+        $this->accountSid = $accountSid;
+        $this->accountToken = $accountToken;
     }
-
 
     /**
      * 设置应用ID
      *
-     * @param AppId 应用ID
+     * @param int $appId 应用ID
      */
-    function setAppId($AppId)
+    function setAppId($appId)
     {
-        $this->AppId = $AppId;
+        $this->appId = $appId;
     }
 
     /**
      * 打印日志
      *
-     * @param log 日志内容
+     * @param string $log 日志内容
      */
-    function showlog($log)
+    function writeLog($log)
     {
-        if ($this->enabeLog) {
-            fwrite($this->Handle, $log . "\n");
-        }
+        if ($this->logEnabled) fwrite($this->handle, "{$log}\n");
     }
 
     /**
      * 发起HTTPS请求
+     * @param string $url
+     * @param mixed $data
+     * @param mixed $header
+     * @param int $post
+     * @return bool|string
      */
     function curl_post($url, $data, $header, $post = 1)
     {
-        //初始化curl
+        // 初始化curl
         $ch = curl_init();
-        //参数设置
-        $res = curl_setopt($ch, CURLOPT_URL, $url);
+        // 参数设置
+        curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_POST, $post);
-        if ($post)
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        if ($post) curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
         $result = curl_exec($ch);
-        //连接失败
-        if ($result == FALSE) {
-            if ($this->BodyType == 'json') {
-                $result = "{\"statusCode\":\"172001\",\"statusMsg\":\"网络错误\"}";
-            } else {
-                $result = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><Response><statusCode>172001</statusCode><statusMsg>网络错误</statusMsg></Response>";
-            }
-        }
-
         curl_close($ch);
-        return $result;
+
+        // 连接失败
+        if ($result != FALSE) return $result;
+
+        if ($this->bodyType == 'json') return "{\"statusCode\":\"172001\",\"statusMsg\":\"网络错误\"}";
+        return "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><Response><statusCode>172001</statusCode><statusMsg>网络错误</statusMsg></Response>";
     }
-
-
+    
     /**
      * 发送模板短信
-     * @param to 短信接收彿手机号码集合,用英文逗号分开
-     * @param datas 内容数据
-     * @param $tempId 模板Id
+     * @param int|string $to 短信接收彿手机号码集合,用英文逗号分开
+     * @param array $dataAry 内容数据
+     * @param int|string $templateId 模板Id
+     * @return mixed|SimpleXMLElement|stdClass
      */
-    function sendTemplateSMS($to, $datas, $tempId)
+    function sendTemplateSMS($to, $dataAry, $templateId)
     {
-        //主帐号鉴权信息验证，对必选参数进行判空。
+        $appId = $this->appId;
+        $datetime = $this->datetime;
+        $bodyType = $this->bodyType;
+        $accountSid = $this->accountSid;
+        $accountToken = $this->accountToken;
+
+        // 主帐号鉴权信息验证，对必选参数进行判空。
         $auth = $this->accAuth();
-        if ($auth != "") {
-            return $auth;
-        }
+        if ($auth != "") return $auth;
+
         // 拼接请求包体
-        if ($this->BodyType == "json") {
-            $data = "";
-            for ($i = 0; $i < count($datas); $i++) {
-                if ($i == 0) {
-                    $data = $data . "'" . $datas[$i] . "'";
-                } else {
-                    $data = $data . ",'" . $datas[$i] . "'";
-                }
-            }
-            $body = "{'to':'$to','templateId':'$tempId','appId':'$this->AppId','datas':[" . $data . "]}";
+        if ($bodyType == "json") {
+            $data = "'" . implode(',', $dataAry) . "'";
+            $body = "{'to':'{$to}','templateId':'{$templateId}','appId':'{$appId}','datas':[{$data}]}";
         } else {
-            $data = "";
-            for ($i = 0; $i < count($datas); $i++) {
-                $data = $data . "<data>" . $datas[$i] . "</data>";
-            }
+            $data = "<data>" . implode('</data><data>', $dataAry) . "</data>";
             $body = "<TemplateSMS>
                     <to>$to</to> 
-                    <appId>$this->AppId</appId>
-                    <templateId>$tempId</templateId>
-                    <datas>" . $data . "</datas>
+                    <appId>{$appId}</appId>
+                    <templateId>{$templateId}</templateId>
+                    <datas>{$data}</datas>
                   </TemplateSMS>";
         }
-        $this->showlog("request body = " . $body);
+        $this->writeLog("request body = {$body}");
+
         // 大写的sig参数 
-        $sig = strtoupper(md5($this->AccountSid . $this->AccountToken . $this->Batch));
+        $sig = strtoupper(md5($accountSid . $accountToken . $datetime));
         // 生成请求URL        
-        $url = "https://$this->ServerIP:$this->ServerPort/$this->SoftVersion/Accounts/$this->AccountSid/SMS/TemplateSMS?sig=$sig";
-        $this->showlog("request url = " . $url);
+        $url = "{$this->apiHost}/Accounts/{$accountSid}/SMS/TemplateSMS?sig={$sig}";
+        $this->writeLog("request url = {$url}");
+
         // 生成授权：主帐户Id + 英文冒号 + 时间戳。
-        $authen = base64_encode($this->AccountSid . ":" . $this->Batch);
+        $authStr = base64_encode("{$accountSid}:{$datetime}");
         // 生成包头  
-        $header = array("Accept:application/$this->BodyType", "Content-Type:application/$this->BodyType;charset=utf-8", "Authorization:$authen");
+        $header = [
+            "Accept:application/{$bodyType}",
+            "Content-Type:application/{$bodyType};charset=utf-8",
+            "Authorization:$authStr"
+        ];
+
         // 发送请求
         $result = $this->curl_post($url, $body, $header);
-        $this->showlog("response body = " . $result);
-        if ($this->BodyType == "json") {//JSON格式
-            $datas = json_decode($result);
-        } else { //xml格式
-            $datas = simplexml_load_string(trim($result, " \t\n\r"));
-        }
-        //  if($datas == FALSE){
-//            $datas = new stdClass();
-//            $datas->statusCode = '172003';
-//            $datas->statusMsg = '返回包体错误'; 
-//        }
-        //重新装填数据
-        if ($datas->statusCode == 0) {
-            if ($this->BodyType == "json") {
-                $datas->TemplateSMS = $datas->templateSMS;
-                unset($datas->templateSMS);
-            }
+        $this->writeLog("response body = {$result}");
+
+        // JSON格式 or xml格式
+        $dataAry = $bodyType == "json" ? json_decode($result) : simplexml_load_string(trim($result, " \t\n\r"));
+
+        // 重新装填数据
+        if ($dataAry->statusCode == 0 && $bodyType == "json") {
+            $dataAry->TemplateSMS = $dataAry->templateSMS;
+            unset($dataAry->templateSMS);
         }
 
-        return $datas;
+        return $dataAry;
     }
-
 
     /**
      * 主帐号鉴权
      */
     function accAuth()
     {
-        if ($this->ServerIP == "") {
+        if ($this->serverHost == "") {
             $data = new stdClass();
             $data->statusCode = '172004';
             $data->statusMsg = 'serverIP为空';
             return $data;
         }
-        if ($this->ServerPort <= 0) {
+        if ($this->serverPort <= 0) {
             $data = new stdClass();
             $data->statusCode = '172005';
             $data->statusMsg = '端口错误（小于等于0）';
             return $data;
         }
-        if ($this->SoftVersion == "") {
+        if ($this->apiVersion == "") {
             $data = new stdClass();
             $data->statusCode = '172013';
             $data->statusMsg = '版本号为空';
             return $data;
         }
-        if ($this->AccountSid == "") {
+        if ($this->accountSid == "") {
             $data = new stdClass();
             $data->statusCode = '172006';
             $data->statusMsg = '主帐号为空';
             return $data;
         }
-        if ($this->AccountToken == "") {
+        if ($this->accountToken == "") {
             $data = new stdClass();
             $data->statusCode = '172007';
             $data->statusMsg = '主帐号令牌为空';
             return $data;
         }
-        if ($this->AppId == "") {
+        if ($this->appId == "") {
             $data = new stdClass();
             $data->statusCode = '172012';
             $data->statusMsg = '应用ID为空';
@@ -217,5 +210,4 @@ class REST
         }
     }
 }
-
 ?>
